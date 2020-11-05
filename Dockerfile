aef3f7ff66b2ed3c974a6785e0bc87e63c97fb3d
sesco <sesco@example.com>
2020-11-05 14:28:38 +0100

    now nginx is working and php & phpmyadmin is working too

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM debian:buster
 
 COPY ./srcs/install.sh /
+COPY ./srcs/default /
 
 RUN bash ./install.sh
--- srcs/install.sh
+++ srcs/install.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-apt-get update && apt-get install build-essential -y
-apt-get install nginx -y
+# Here we install the basic packages
 
-/bin/bash
+apt-get update && apt-get install build-essential -y
+
+# And here we install nginx
+
+apt-get install nginx -y
+mv -f /default /etc/nginx/sites-available/
+
+# Installing wget command
+
+apt-get install -y wget
+
+rm -rf /var/lib/apt/list/*
+
+# Installing phpmyadmin and php
+
+apt-get install php php-fpm php-mysql php-gettext php-mbstring -y
+
+wget https://files.phpmyadmin.net/phpMyAdmin/4.9.7/phpMyAdmin-4.9.7-english.tar.xz
+
+tar -xf phpMyAdmin-4.9.7-english.tar.xz
+
+mv phpMyAdmin-4.9.7-english phpmyadmin
+
+mv /phpmyadmin /var/www/html
+
+# Running some services
+
+nginx
+
+service php7.3-fpm start
